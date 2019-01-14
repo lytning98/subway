dee3f634135446d0ff8229449fc2c7c8dfce4730
lytning <lytning@bitnp.net>
2019-01-14 20:59:27 +0800

    完成主函数参数处理

--- subway/Program.cs
+++ subway/Program.cs
@@ -8,21 +8,61 @@ using System.Windows.Forms;
 
 namespace subway
 {
+
+    using Graph;
+
     class Program
     {
+
         [STAThread]
         static void Main(string[] args)
         {
             if (!Map.InitMap("subway_map.json"))
                 return;
-
-            if(args.Length == 0)
+            GraphSolver.BuildGraph();
+            if (args.Length == 3 && args[0] == "/b")
+            {
+                if(!Map.StationId.ContainsKey(args[1]) ||
+                    !Map.StationId.ContainsKey(args[2]))
+                {
+                    Console.WriteLine("站名有误或数据文件未收录此站！");
+                    return;
+                }
+                int id1 = Map.StationId[args[1]], id2 = Map.StationId[args[2]];
+                var res = GraphSolver.CalcPath(id1, id2);
+                Console.WriteLine(res.ToString());
+                return;
+            }
+            if(args.Length == 2 && args[0] == "/a")
+            {
+                if(!Map.StationId.ContainsKey(args[1]))
+                {
+                    Console.WriteLine("站名有误或数据文件未收录此站！");
+                    return;
+                }
+                int id = Map.StationId[args[1]];
+                var res = GraphSolver.TravelAroundPath(id);
+                Console.WriteLine(res.ToPathString(false));
+                return;
+            }
+            if(args.Length == 2 && args[0] == "/z")
+            {
+                LineQuery.TestAnswer(args[1]);
+                return;
+            }
+            if(args.Length == 1 && args[0] == "/g")
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
+                Application.Run(new GUIForm());
+                return;
             }
-            Console.WriteLine("started in console mode");
+            if(args.Length == 0)
+            {
+                Console.WriteLine("当前以无参数方式启动.");
+                LineQuery.LineStations();
+            }
+            
         }
     }
 }
